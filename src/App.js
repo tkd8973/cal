758cded0bcf181ae45925f1c0eeefe4d9f4ade1d
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-07-06 15:12:06 +0900

    📆틀 만들기

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+
+import React from 'react';
+import Calendar from './Calendar';
+import './assets/styles/common/_common.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
